114ac83f55d3d1a50d87af66baf81a49c633d0f6
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-11-06 13:06:42 +0000

    create model for issue and issues add dependency for plugin

--- src/main/java/com/myhttpakka/model/Issues.java
+++ src/main/java/com/myhttpakka/model/Issues.java
@@ -1,4 +1,22 @@
 package com.myhttpakka.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Issues {
+
+    private final List<Issue> issues;
+
+    public Issues(){
+
+        this.issues = new ArrayList<Issue>();
+    }
+
+    public Issues(List<Issue> issues){
+        this.issues = issues;
+    }
+
+    public List<Issue> getIssues() {
+        return issues;
+    }
 }
